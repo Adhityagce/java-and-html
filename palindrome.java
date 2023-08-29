479ed8ff65ae2bae2a937d635a4c59da7819f0ca
the-red777 <jayabalan2475@gmail.com>
2023-08-29 23:22:49 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ class PalindromeProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
