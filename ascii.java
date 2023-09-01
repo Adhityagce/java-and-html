e0d8643912d80c4db1b46b2fe625c9bbbc83446d
Adhityagce <mvadhitya@gmail.com>
2023-09-01 22:47:37 +0530

    ascii.java

--- ascii.java
+++ ascii.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;  
 public class AsciiValue {
 
     public static void main(String[] args) {
-
-        char ch = 'a';
+     Scanner d = new Scanner(System.in); 
+        char ch ;
+        ch=d.next().charAt(0);
         int ascii = ch;
+        // You can also cast char to int
         int castAscii = (int) ch;
 
         System.out.println("The ASCII value of " + ch + " is: " + ascii);
